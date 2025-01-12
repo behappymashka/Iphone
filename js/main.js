e2dc2a55ad4b732f69f30e695a6e95fea6a541ac
behappymashka <98Mashenka@gmail.com>
2025-01-12 18:30:09 +0200

    any fixed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 const btn = document.querySelector('.menu-btn');
 const nav = document.querySelector('.nav');
+const body = document.body;
+
 
 btn.addEventListener('click', (e) => {
-   nav.classList.toggle('menu-open');
+    nav.classList.toggle('menu-open');
+});
+
+
+
+btn.addEventListener('click', () => {
+    nav.classList.toggle('menu-open');
+    body.classList.toggle('menu-open');
 });
